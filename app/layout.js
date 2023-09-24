f0e432fcb87f6f1f5da7140da61f385245560a98
mhassankhanw3 <mhassankhanw3@gmail.com>
2023-09-24 16:18:51 +0500

    use client

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-"use client";
+// "use client";gi
 import Nav from "@components/Nav";
 import "@styles/globals.css";
 import { Providers } from "./providers";
 import SessionProvider from "@components/Provider";
 import { Toaster } from "react-hot-toast";
 
-export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
-};
+// export const metadata = {
+//   title: "Promptopia",
+//   description: "Discover & Share AI Prompts",
+// };
 
 export default function RootLayout({ children }) {
   return (
